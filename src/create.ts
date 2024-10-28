cc6b768604fc6e62990fece4dbc16f38480d94b6
ibnabdullah1 <arfathossen541@gmail.com>
2024-10-28 10:23:51 +0600

    insert data

--- src/create.ts
+++ src/create.ts
@@ -11,43 +11,94 @@ const main = async () => {
   //     },
   //   });
   //   console.log(result);
-
   //   @ createMany
-  const createMany = await prisma.posts.createMany({
-    data: [
-      {
-        title: "My First Post 1",
-        content: "This is the content of my first post.",
-        published: true,
-        author: "Arafat",
-      },
-      {
-        title: "My First Post 2",
-        content: "This is the content of my first post.",
-        published: false,
-        author: "Arafat",
-      },
-      {
-        title: "My First Post 3",
-        content: "This is the content of my first post.",
-        published: false,
-        author: "Arafat",
-      },
-      {
-        title: "My First Post 4",
-        content: "This is the content of my first post.",
-        published: false,
-        author: "Arafat",
-      },
-      {
-        title: "My First Post 5",
-        content: "This is the content of my first post.",
-        published: false,
-        author: "Arafat",
-      },
-    ],
-  });
+  // const createMany = await prisma.posts.createMany({
+  //   data: [
+  //     {
+  //       title: "My First Post 1",
+  //       content: "This is the content of my first post.",
+  //       published: true,
+  //       author: "Arafat",
+  //     },
+  //     {
+  //       title: "My First Post 2",
+  //       content: "This is the content of my first post.",
+  //       published: false,
+  //       author: "Arafat",
+  //     },
+  //     {
+  //       title: "My First Post 3",
+  //       content: "This is the content of my first post.",
+  //       published: false,
+  //       author: "Arafat",
+  //     },
+  //     {
+  //       title: "My First Post 4",
+  //       content: "This is the content of my first post.",
+  //       published: false,
+  //       author: "Arafat",
+  //     },
+  //     {
+  //       title: "My First Post 5",
+  //       content: "This is the content of my first post.",
+  //       published: false,
+  //       author: "Arafat",
+  //     },
+  //   ],
+  // });
+  // console.log(createMany);
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "user1",
+  //     email: "user1@ph.com",
+  //     role: Role.user,
+  //   },
+  // });
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "this is bio...",
+  //     userId: 1,
+  //   },
+  // });
+  // console.log(createProfile);
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "software engineering",
+  //   },
+  // });
 
-  console.log(createMany);
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is title 2",
+      content: "this is content of the post. 2",
+      authorId: 1,
+      postCategory: {
+        create: {
+          category: {
+            connect: {
+              id: 3,
+            },
+          },
+        },
+        // create: [
+        //   {
+        //     categoryId: 1,
+        //   },
+        //   {
+        //     categoryId: 3,
+        //   },
+        //   {
+        //     categoryId: 4,
+        //   },
+        // ],
+      },
+    },
+    include: {
+      postCategory: true,
+    },
+  });
+  console.log(createPost);
+
+  // console.log(createCategory);
 };
 main();
